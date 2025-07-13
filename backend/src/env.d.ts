63410c75dadd9838f476416df29a17f88aff1157
Hamid Javadi <javadi.business@gmail.com>
2025-07-13 19:11:56 +0300

    MongoDb has been configured

--- backend/src/env.d.ts
+++ backend/src/env.d.ts
@@ -1,7 +1,10 @@
+import { NodeENV } from './config/types';
+
 declare namespace NodeJS {
   interface ProcessEnv {
     NASA_API_KEY: string;
     PORT?: string;
-    NODE_ENV: 'development' | 'production';
+    NODE_ENV: NodeENV;
+    MONGO_DB_CONNECTION_STRING: string;
   }
 }
